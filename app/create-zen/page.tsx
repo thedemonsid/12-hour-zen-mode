40769ea22e6164de240520ea6539907dd17839aa
thedemonsid <siddheshshrirame@gmail.com>
2024-10-31 22:06:54 +0530

    pheonix

--- app/create-zen/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { CreateZenPageComponent } from "@/components/create-zen-page";
-const CraeteZen = () => {
-  return <CreateZenPageComponent></CreateZenPageComponent>;
-};
-
-export default CraeteZen;
